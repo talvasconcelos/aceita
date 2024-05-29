cce9dd5d3446baae3688317e6ae7d101639b82cf
miguelalmeida2 <miguelalmeida991@protonmail.com>
2024-05-29 08:51:57 +0100

    add Event done in Madeira

--- src/pages/Events.svelte
+++ src/pages/Events.svelte
@@ -1,5 +1,5 @@
 <script>
-  import EventCard from '../components/EventCard.svelte';
+  import EventCard from "../components/EventCard.svelte";
 </script>
 
 <section class="container grid-lg">
@@ -10,39 +10,39 @@
     </div>
   </header>
   <div class="card-grid">
-  <EventCard
-    title="Peniche - A.E.F.C.R.P."
-    subtitle="Workshop Bitcoin para comércio local"
-    address="A.E.F.C.R.P., Av. das Escolas, 2520-294 Peniche"
-    date="Maio"
-    time="17h - 19h"
-    img={null}
-    description="Workshop para explicar os fundamentos de Bitcoin e como este novo sistema monetário pode fazer mais por Peniche."
-    signinUrl={null}
-    btnText="Saber Mais"
-  />
-  <EventCard
-    title="Óbidos"
-    subtitle="Bitcoin - Moeda Universal"
-    address="Parque Tecnológico Óbidos, Edifícios Centrais, Rua da Criatividade, 2510-216 Óbidos"
-    date="28 Abril 2022"
-    time="11h - 13h"
-    img={null}
-    description="Sessão/debate, em formato híbrido, para explicar os fundamentos de Bitcoin: o que é o dinheiro, como funciona esta moeda digital e como se transacciona, por que razão é tão interessante e que dimensões resolve."
-    signinUrl={null}
-    btnText="Saber Mais"
-  />
-  <EventCard
-    title="Caldas da Rainha"
-    subtitle="Workshop Bitcoin e Prontos."
-    address="Avenida Primeiro de Maio, 20 A, 2500-081 Caldas da Rainha"
-    date="23 Março"
-    time="18h - 20h"
-    img="../../events/ev_04.jpg"
-    description="Workshop Bitcoin na Prontos em Caldas da Rainha. Metade do valor da inscrição é reembolsada em bitcoin no dia do evento. Venha conhecer Bitcoin e a Lightning Network!"
-    signinUrl="https://forms.gle/djycWjpaeUsip6ot8"
-    btnText="Saber Mais"
-  />
+    <EventCard
+      title="Peniche - A.E.F.C.R.P."
+      subtitle="Workshop Bitcoin para comércio local"
+      address="A.E.F.C.R.P., Av. das Escolas, 2520-294 Peniche"
+      date="Maio"
+      time="17h - 19h"
+      img={null}
+      description="Workshop para explicar os fundamentos de Bitcoin e como este novo sistema monetário pode fazer mais por Peniche."
+      signinUrl={null}
+      btnText="Saber Mais"
+    />
+    <EventCard
+      title="Óbidos"
+      subtitle="Bitcoin - Moeda Universal"
+      address="Parque Tecnológico Óbidos, Edifícios Centrais, Rua da Criatividade, 2510-216 Óbidos"
+      date="28 Abril 2022"
+      time="11h - 13h"
+      img={null}
+      description="Sessão/debate, em formato híbrido, para explicar os fundamentos de Bitcoin: o que é o dinheiro, como funciona esta moeda digital e como se transacciona, por que razão é tão interessante e que dimensões resolve."
+      signinUrl={null}
+      btnText="Saber Mais"
+    />
+    <EventCard
+      title="Caldas da Rainha"
+      subtitle="Workshop Bitcoin e Prontos."
+      address="Avenida Primeiro de Maio, 20 A, 2500-081 Caldas da Rainha"
+      date="23 Março"
+      time="18h - 20h"
+      img="../../events/ev_04.jpg"
+      description="Workshop Bitcoin na Prontos em Caldas da Rainha. Metade do valor da inscrição é reembolsada em bitcoin no dia do evento. Venha conhecer Bitcoin e a Lightning Network!"
+      signinUrl="https://forms.gle/djycWjpaeUsip6ot8"
+      btnText="Saber Mais"
+    />
     <EventCard
       title="Viseu"
       subtitle="Workshop Bitcoin & Lightning Network"
@@ -51,6 +51,16 @@
       img="../../events/workshop_viseu_2021.jpg"
       description="Venha conhecer o que é Bitcoin, como funciona e como pode começar a usar, para fazer e receber pagamentos"
       signinUrl="https://docs.google.com/forms/d/12OEC3sFbmnPR6XwABOPJUQvVTMrfrXKv4i8U_e-qamI"
+    />
+    <EventCard
+      title="Madeira"
+      subtitle="Bitcoin Atlantis - Bem-vindo ao Bitcoin"
+      date="29 Fevereiro"
+      time="9h - 17h"
+      img="../../events/CCIF.jpg"
+      description="Descobre o fascinante universo do Bitcoin no evento “Bem-vindo ao Bitcoin”, uma iniciativa da F.R.E.E. Madeira, em parceria com o grupo Aceita Bitcoin."
+      signinUrl="https://bitcoinatlantis.com/bem-vindo-ao-bitcoin"
+      btnText="Saber Mais"
     />
   </div>
 </section>
